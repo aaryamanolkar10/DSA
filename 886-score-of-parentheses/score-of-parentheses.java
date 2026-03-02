e4467c877c7fa8a02ff0109fdaeea2f96b028f17
aaryamanolkar10 <aaryamanolkar10@gmail.com>
2026-03-02 14:28:04 +0530

    Time: 1 ms (55.93%) | Memory: 42.7 MB (69.83%) - LeetSync

---
+++ 886-score-of-parentheses/score-of-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int scoreOfParentheses(String s) {
+        Stack<Integer> stack=new Stack<>();
+        stack.push(0);
+        for(char c: s.toCharArray()){
+            if(c=='('){
+                stack.push(0);
+            }else{
+                int current=stack.pop();
+                int previous=stack.pop();
+                int score=previous+Math.max(2*current,1);
+                stack.push(score);
+            }
+
+        }
+        return stack.pop();
+        
+    }
+}
