3ee40df8067348a1ade6e6767fd33de3fcf97cc9
Aarya Manolkar <aaryamanolkar10@gmail.com>
2026-04-09 19:14:25 +0530

    Time: 1 ms (88.90%) | Memory: 43.3 MB (53.74%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int caps=0,low=0,first=0;
+        for(char ch:word.toCharArray()){
+            if(ch>=65 && ch<=90){
+                caps++;
+                if(first==0){
+                    first++;
+                }
+            }else{
+                low++;
+                if(first==0){
+                    first=2;
+                }
+            }
+
+            
+        }
+        if(caps==0 || low==0 ||first==1 && caps==1){
+            return true;
+        }
+        return false;
+    }
+}
