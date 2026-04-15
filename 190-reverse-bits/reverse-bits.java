ae01dec37ecbcc2502a5dde8aa5b1d10ab145bee
Aarya Manolkar <aaryamanolkar10@gmail.com>
2026-04-15 16:59:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (45.67%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            result = result << 1;      // shift result left
+            result = result | (n & 1); // add last bit of n
+            n = n >> 1;               // shift n right
+        }
+
+        return result;
+    }
+}
