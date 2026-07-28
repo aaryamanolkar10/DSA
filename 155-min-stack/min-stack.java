6cc54c8f787290ba2c3d0e67bff5abdb115058f4
Aarya Manolkar <aaryamanolkar10@gmail.com>
2026-07-28 18:48:02 +0530

    Time: 38 ms (9.19%) | Memory: 99.6 MB (80.09%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,47 +1,29 @@
-import java.util.Stack;
-
 class MinStack {
-
     Stack<Integer> stack;
-    Stack<Integer> minStack;
-
-    // Constructor
+    Stack<Integer> minstack;
     public MinStack() {
-
-        stack = new Stack<>();
-        minStack = new Stack<>();
+        stack=new Stack<>();
+        minstack=new Stack<>();
     }
-
-    // Push element
+    
     public void push(int val) {
-
         stack.push(val);
-
-        // push into minStack if empty or val <= current min
-        if(minStack.isEmpty() || val <= minStack.peek()){
-            minStack.push(val);
-        }
+        if(minstack.isEmpty() || val<=minstack.peek())
+            minstack.push(val);
     }
-
-    // Pop element
+    
     public void pop() {
-
-        int removed = stack.pop();
-
-        if(removed == minStack.peek()){
-            minStack.pop();
+        int poppedvalue=stack.pop();
+        if(poppedvalue==minstack.peek() && !minstack.isEmpty()){
+            minstack.pop();
         }
     }
-
-    // Get Top element
+    
     public int top() {
-
         return stack.peek();
     }
-
-    // Get Minimum element
+    
     public int getMin() {
-
-        return minStack.peek();
+        return minstack.peek();
     }
 }
