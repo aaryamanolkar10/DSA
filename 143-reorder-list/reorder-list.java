c198f7596b2938b8a18a3df9b3a5ef1810e1bd57
Aarya Manolkar <aaryamanolkar10@gmail.com>
2026-08-08 14:32:37 +0530

    Time: 2 ms (87.34%) | Memory: 49.2 MB (54.32%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -1,32 +1,34 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public void reorderList(ListNode head) {
-        if (head == null || head.next == null || head.next.next == null)
+        //base case
+        if (head == null || head.next == null)
             return;
 
-        ListNode last = head.next;
-        ListNode secondLast = head;
-
-        while (last.next != null) {
-            last = last.next;
-            secondLast = secondLast.next;
+        //finding middle of LL
+        ListNode p1 = head, p2 = head;
+        while (p2.next != null && p2.next.next != null) {
+            p2 = p2.next.next;
+            p1 = p1.next;
         }
 
-        ListNode temp = head.next;
-        head.next = last;
-        last.next = temp;
-        secondLast.next = null;
+        //reversing the second half
+        ListNode preMiddle = p1, preCurrent = p1.next;
+        while (preCurrent.next != null) {
+            ListNode current = preCurrent.next;
+            preCurrent.next = current.next;
+            current.next = preMiddle.next;
+            preMiddle.next = current;
+        }
 
-        reorderList(temp);
-
+        //joining the reversed half and the first half
+        p1 = head;
+        p2 = preMiddle.next;
+        while (p1 != preMiddle) {
+            preMiddle.next = p2.next;
+            p2.next = p1.next;
+            p1.next = p2;
+            p1 = p2.next;
+            p2 = preMiddle.next;
+        }
     }
 }
