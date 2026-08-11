304518b1989776eac2013834a8297c3a43f58209
Aarya Manolkar <aaryamanolkar10@gmail.com>
2026-08-11 19:58:57 +0530

    Time: 1 ms (63.87%) | Memory: 45.2 MB (94.50%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<indices.length;i++){
+            sb.append(" ");
+        }
+        for(int i=0;i<s.length();i++){
+            sb.setCharAt(indices[i],s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
